b62d01148d1aa486a599df8071a7eb4459e2c892
asilva-juarez <asilva-juarez@myseneca.ca>
2024-05-28 04:06:22 -0400

    Handling error left

---
+++ utils.js
@@ -0,0 +1,15 @@
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
+
+function metersPerSecondToMilesPerHour(mps) {
+  return mps * 2.23694;
+}
+
+function milesPerHourToMetersPerSecond(mph) {
+  return mph * 0.44704;
+}
