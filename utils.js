bbabe9e4849699809c1dc39b17616066d5f5fc7d
asilva-juarez <asilva-juarez@myseneca.ca>
2024-05-28 16:07:46 -0400

    Sunrise and sunset completed

--- utils.js
+++ utils.js
@@ -13,3 +13,11 @@ function metersPerSecondToMilesPerHour(mps) {
 function milesPerHourToMetersPerSecond(mph) {
   return mph * 0.44704;
 }
+
+// Function to convert Unix timestamp to human-readable format
+function convertTimestampToTime(timestamp) {
+  const date = new Date(timestamp * 1000);
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  return `${hours}:${minutes}`;
+}
