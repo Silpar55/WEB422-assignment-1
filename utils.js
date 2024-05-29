7b797ddb2cf04ff3d8fa2378e9c00f3de89eba3f
asilva-juarez <asilva-juarez@myseneca.ca>
2024-05-28 23:04:05 -0400

    Assignment 1 finished

--- utils.js
+++ utils.js
@@ -1,3 +1,14 @@
+/**************************
+*
+•⁠  ⁠WEB422 – Assignment 1
+•⁠  ⁠I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
+•⁠  ⁠No part of this assignment has been copied manually or electronically from any other source
+•⁠  ⁠(including web sites) or distributed to other students.
+* 
+•⁠  ⁠Name: Alejandro Student ID: 142655224 Date: 2024-05-28
+*
+**************************
+**/
 function celsiusToFahrenheit(celsius) {
   return (celsius * 9) / 5 + 32;
 }
@@ -14,10 +25,10 @@ function milesPerHourToMetersPerSecond(mph) {
   return mph * 0.44704;
 }
 
-// Function to convert Unix timestamp to human-readable format
-function convertTimestampToTime(timestamp) {
-  const date = new Date(timestamp * 1000);
-  const hours = date.getHours().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
+// Function to convert Unix timestamp to human-readable format considering timezone offset
+function convertTimestampToTime(timestamp, timezoneOffset) {
+  const date = new Date((timestamp + timezoneOffset) * 1000); // Adjust timestamp with timezone offset
+  const hours = date.getUTCHours().toString().padStart(2, "0");
+  const minutes = date.getUTCMinutes().toString().padStart(2, "0");
   return `${hours}:${minutes}`;
 }
